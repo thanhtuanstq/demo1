0b004d6731ac9436153ed63924fe4711dbdeb189
thanhtuanstq <thanhtuanstq@gmail.com>
2021-09-13 09:30:45 +0700

    commit dau tien

---
+++ mvc/src/main/java/demo1/mvc/services/TodoListService.java
@@ -0,0 +1,32 @@
+/* 
+	Description:
+		ZK Essentials
+	History:
+		Created by dennis
+
+Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+*/
+package demo1.mvc.services;
+
+import java.util.List;
+
+import demo1.mvc.entity.Todo;
+
+public interface TodoListService {
+
+	/** get Todo list **/
+	List<Todo> getTodoList();
+	
+	/** get Todo by id **/
+	Todo getTodo(Integer id);
+	
+	/** save Todo **/
+	Todo saveTodo(Todo todo);
+	
+	/** update Todo **/
+	Todo updateTodo(Todo todo);
+	
+	/** delete Todo **/
+	void deleteTodo(Todo todo);
+	
+}
